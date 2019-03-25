5ff7ca1d1d0a191a36be2669b4a0d298a1e1c4e3
lmn <liumengna_mail@163.com>
2019-03-25 21:03:21 +0800

    滑动窗口的最大值

--- src/_65maxSlideWindow/MaxWindow.java
+++ src/_65maxSlideWindow/MaxWindow.java
@@ -10,46 +10,39 @@ public class MaxWindow {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] nums = {2,4,1,4,6,8,5,3,4,6};
-		int winLen = 3;
+		int[] nums = {2, 3, 4, 2, 6, 2, 5, 1};
 		List<Integer> list = new ArrayList<Integer>();
-		List<Integer> res = new ArrayList<Integer>();
-		findMaxWin(nums, winLen, list, res);
-		System.out.println("result is: ");
-		for (Integer integer : res) {
-			System.out.println(nums[integer]+" ");
-		}
+		findMax(nums, list, 3);
 	}
 
-	private static void findMaxWin(int[] nums, int winLen, List<Integer> list, List<Integer> res) {
+	private static void findMax(int[] nums, List<Integer> list, int size) {
 		// TODO Auto-generated method stub
-		for (int i = 0; i < winLen; i++) {
-//			System.out.println(i);
-			while (!list.isEmpty()) {
-//				System.out.println("size"+list.size());
-//				System.out.println(list.toString());
-//				System.out.println("pp" + list.get(list.size()-1));
-				if (nums[i] >= nums[list.get(list.size()-1)]) {
-					
-					list.remove(list.size()-1);
-					
-				}else {
-					break;
-				}
+	    List<Integer> res = new ArrayList<>();
+		if (nums == null || size < 0 || size > nums.length) {
+			return;
+		}
+		for (int i = 0; i < size; i++) {
+			while (!list.isEmpty() && nums[i] >= nums[list.get(0)]) {
+				list.remove(0);
 			}
 			list.add(i);
 		}
-		for (int i = winLen; i < nums.length; i++) {
+		
+		for (int i = size; i < nums.length; i++) {
 			res.add(list.get(0));
-			while (!list.isEmpty() && nums[i] > nums[list.get(list.size()-1)]) {
+			while (!list.isEmpty() && nums[i] >= nums[list.get(list.size()-1)]) {
 				list.remove(list.size()-1);
 			}
-			if (!list.isEmpty() && (i - winLen)>=list.get(0) ) {
+			if (!list.isEmpty() && list.get(0) <= (i - size)) {
 				list.remove(0);
 			}
 			list.add(i);
 		}
 		res.add(list.get(0));
+		for (Integer integer : res) {
+			System.out.println(nums[integer]);
+		}
+		System.out.println("------------------");
 	}
 
 }
